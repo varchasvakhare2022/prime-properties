7eab7c89dc03999f90f1984d3e8669ffae5198a3
varchasvakhare2022 <112934587+varchasvakhare2022@users.noreply.github.com>
2025-10-19 11:58:11 +0530

    Remove Scroll Indicator

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -193,34 +193,6 @@ const Home = () => {
             </div>
           </motion.div>
 
-          {/* Scroll Indicator */}
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1.5 }}
-            className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-          >
-            <motion.div
-              animate={{ y: [0, 10, 0] }}
-              transition={{ duration: 2, repeat: Infinity }}
-              className="flex flex-col items-center gap-2 text-gray-400"
-            >
-              <span className="text-sm">Scroll to explore</span>
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 14l-7 7m0 0l-7-7m7 7V3"
-                />
-              </svg>
-            </motion.div>
-          </motion.div>
         </div>
       </section>
 
